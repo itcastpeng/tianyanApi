ae7252fb90c6fd1e9271fd503de3cf270c335df3
congzhang <652314341@qq.com>
2019-02-11 17:21:21 +0800

    增加获取团队成员列表功能

--- api/forms/team.py
+++ api/forms/team.py
@@ -53,6 +53,45 @@ class UpdateForm(forms.Form):
 
 # 查询
 class SelectForm(forms.Form):
+    team_id = forms.IntegerField(
+        required=True,
+        error_messages={
+            'invalid': "页码数据类型错误",
+            'required': "团队id不能为空"
+        }
+    )
+
+    current_page = forms.IntegerField(
+        required=False,
+        error_messages={
+            'invalid': "页码数据类型错误"
+        }
+    )
+
+    length = forms.IntegerField(
+        required=False,
+        error_messages={
+            'invalid': "页显示数量类型错误"
+        }
+    )
+
+    def clean_current_page(self):
+        if 'current_page' not in self.data:
+            current_page = 1
+        else:
+            current_page = int(self.data['current_page'])
+        return current_page
+
+    def clean_length(self):
+        if 'length' not in self.data:
+            length = 10
+        else:
+            length = int(self.data['length'])
+        return length
+
+
+# 查看团队人员列表
+class SelectUserList(forms.Form):
     current_page = forms.IntegerField(
         required=False,
         error_messages={
--- api/views_dir/team.py
+++ api/views_dir/team.py
@@ -137,37 +137,65 @@ def team_oper(request, oper_type, o_id):
                 #  字符串转换 json 字符串
                 response.msg = json.loads(forms_obj.errors.as_json())
 
-        # 修改文章所属分类
-        elif oper_type == "update_classify":
-            form_data = {
-                'o_id': o_id,  # 文章id
-                'create_user_id': user_id,
-                'classify_id': request.POST.get('classify_id'),
-            }
-
-            forms_obj = UpdateClassifyForm(form_data)
-            if forms_obj.is_valid():
-                print("验证通过")
-                print(forms_obj.cleaned_data)
-                o_id = forms_obj.cleaned_data['o_id']
-                classify_id = forms_obj.cleaned_data['classify_id']
-
-                #  查询更新 数据
-                models.Article.objects.filter(id=o_id).update(
-                    classify_id=classify_id,
-                )
-                response.code = 200
-                response.msg = "修改成功"
-
-            else:
-                print("验证不通过")
-                # print(forms_obj.errors)
-                response.code = 301
-                # print(forms_obj.errors.as_json())
-                #  字符串转换 json 字符串
-                response.msg = json.loads(forms_obj.errors.as_json())
     else:
-        response.code = 402
-        response.msg = "请求异常"
+        # 查看团队人员列表
+        if oper_type == "select_user_list":
+            form_data = {
+                'team_id': o_id,
+                'current_page': request.GET.get('current_page'),
+                'length': request.GET.get('length'),
+            }
+            forms_obj = SelectForm(form_data)
+            if forms_obj.is_valid():
+                current_page = forms_obj.cleaned_data['current_page']
+                length = forms_obj.cleaned_data['length']
+                team_id = forms_obj.cleaned_data['team_id']
+                print('forms_obj.cleaned_data -->', forms_obj.cleaned_data)
+                order = request.GET.get('order', 'create_datetime')
+                # field_dict = {
+                #     'id': '',
+                #     'name': '__contains',
+                #     'create_datetime': '',
+                # }
+                # q = conditionCom(request, field_dict)
+                # classify_type = forms_obj.cleaned_data.get('classify_type')    # 分类类型，1 => 推荐, 2 => 品牌
+
+                team_obj = models.Team.objects.get(id=team_id)
+                objs = team_obj.userprofile_team.all().order_by(order)
+                count = objs.count()
+
+                if length != 0:
+                    start_line = (current_page - 1) * length
+                    stop_line = start_line + length
+                    objs = objs[start_line: stop_line]
+
+                # 返回的数据
+                ret_data = []
+
+                for obj in objs:
+                    #  将查询出来的数据 加入列表
+                    ret_data.append({
+                        'id': obj.id,
+                        'name': obj.name,
+                        'set_avator': obj.set_avator,
+                        'create_datetime': obj.create_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+                    })
+                #  查询成功 返回200 状态码
+                response.code = 200
+                response.msg = '查询成功'
+                response.data = {
+                    'ret_data': ret_data,
+                    'data_count': count,
+                }
+
+                response.note = {
+                    'id': "用户id",
+                    'name': "用户名称",
+                    'set_avator': "用户头像",
+                    'create_datetime': "用户加入时间"
+                }
+            else:
+                response.code = 301
+                response.data = json.loads(forms_obj.errors.as_json())
 
     return JsonResponse(response.__dict__)
