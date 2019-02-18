67424a471e69ddcf2f3dcc5dceda0c79733a7cb9
zhaoxinpeng <15531506906@sina.cn>
2019-02-18 09:55:34 +0800

    支付 回调

--- api/views_dir/prepaidManagement.py
+++ api/views_dir/prepaidManagement.py
@@ -6,159 +6,96 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 
+class pub(object):
+    def __init__(self):
+        pass
 
-response = Response.ResponseObj()
-def md5(string):
-    m = hashlib.md5()
-    m.update(string.encode('utf8'))
-    return m.hexdigest()
+    def md5(self, string):
+        m = hashlib.md5()
+        m.update(string.encode('utf8'))
+        return m.hexdigest()
 
-# 返回 xml
-def toXml(params):
-    xml = []
-    for k in sorted(params.keys()):
-        v = params.get(k)
-        if k == 'detail' and not v.startswith('<![CDATA['):
-            v = '<![CDATA[{}]]>'.format(v)
-        xml.append('<{key}>{value}</{key}>'.format(key=k, value=v))
-    return '<xml>{}</xml>'.format(''.join(xml))
+    # 返回 xml
+    def toXml(self, params):
+        xml = []
+        for k in sorted(params.keys()):
+            v = params.get(k)
+            if k == 'detail' and not v.startswith('<![CDATA['):
+                v = '<![CDATA[{}]]>'.format(v)
+            xml.append('<{key}>{value}</{key}>'.format(key=k, value=v))
+        return '<xml>{}</xml>'.format(''.join(xml))
 
-# 返回32为 时间戳
-def generateRandomStamping():
-    return str(uuid.uuid4()).replace('-', '')
+    # 返回32为 时间戳
+    def generateRandomStamping(self):
+        return str(uuid.uuid4()).replace('-', '')
 
-# 生成二维码
-# def create_qrcode(url):
-#     img = qrcode.make(url)
-#     img.get_image().show()
-#     img.save('hello.png')
+    # 生成二维码
+    # def create_qrcode(self, url):
+    #     img = qrcode.make(url)
+    #     img.get_image().show()
+    #     img.save('hello.png')
 
-# 生成 签名
-def shengchengsign(result_data, KEY=None):
-    ret = []
-    for k in sorted(result_data.keys()):
-        if (k != 'sign') and (k != '') and (result_data[k] is not None):
-            ret.append('%s=%s' % (k, result_data[k]))
-    stringA = '&'.join(ret)
-    stringSignTemp = stringA
-    if KEY:
-        stringSignTemp = '{stringA}&key={key}'.format(
-            stringA=stringA,
-            key=KEY
-        )
-    return stringSignTemp
-
+    # 生成 签名
+    def shengchengsign(self, result_data, KEY=None):
+        ret = []
+        for k in sorted(result_data.keys()):
+            if (k != 'sign') and (k != '') and (result_data[k] is not None):
+                ret.append('%s=%s' % (k, result_data[k]))
+        stringA = '&'.join(ret)
+        stringSignTemp = stringA
+        if KEY:
+            stringSignTemp = '{stringA}&key={key}'.format(
+                stringA=stringA,
+                key=KEY
+            )
+        return stringSignTemp
 
 
-@csrf_exempt
-def wxpay(request):
-    print('------------------------------回调')
-    # resultBody = request.body
-#     print('resultBody------------------> ',resultBody)
-#     DOMTree = xmldom.parseString(resultBody)
-#     collection = DOMTree.documentElement
-#     data = ['mch_id', 'return_code', 'appid', 'openid', 'cash_fee', 'out_trade_no']
-#     resultData = xmldom_parsing.xmldom(collection, data)
-#
-#     # mch_id = collection.getElementsByTagName("mch_id")[0].childNodes[0].data            # 商户号
-#     # return_code = collection.getElementsByTagName("return_code")[0].childNodes[0].data  # 状态
-#     # appid = collection.getElementsByTagName("appid")[0].childNodes[0].data              # 小程序appid
-#     # openid = collection.getElementsByTagName("openid")[0].childNodes[0].data            # 用户openid
-#     # cash_fee = collection.getElementsByTagName("cash_fee")[0].childNodes[0].data        # 钱数
-#     # out_trade_no = collection.getElementsByTagName("out_trade_no")[0].childNodes[0].data# 订单号
-#
-#
-#     dingDanobjs = models.zgld_shangcheng_dingdan_guanli.objects.select_related('shangpinguanli',).filter(orderNumber=resultData['out_trade_no'],theOrderStatus=1)
-#     print('=========================回调订单号===============================> ', resultData['out_trade_no'])
-#     nowDate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#     print('=-------------------------当前时间---------------------->', nowDate, type(nowDate))
-#
-#     if dingDanobjs:
-#         if resultData['return_code'] == 'SUCCESS':
-#             if dingDanobjs:
-#                 # 二次 查询是否付款成功
-#                 result_data = {
-#                     'appid': resultData['appid'],                 # appid
-#                     'mch_id': resultData['mch_id'],               # 商户号
-#                     'out_trade_no': resultData['out_trade_no'],   # 订单号
-#                     'nonce_str': generateRandomStamping(),  # 32位随机值
-#                 }
-#                 url = 'https://api.mch.weixin.qq.com/pay/orderquery'
-#                 objs = models.zgld_shangcheng_jichushezhi.objects.select_related('xiaochengxuApp').filter(xiaochengxuApp__authorization_appid=resultData['appid'])
-#                 SHANGHUKEY = objs[0].shangHuMiYao
-#                 company_id = objs[0].xiaochengxucompany_id
-#
-#                 stringSignTemp = shengchengsign(result_data, SHANGHUKEY)
-#                 result_data['sign'] = md5(stringSignTemp).upper()
-#                 xml_data = toXml(result_data)
-#                 ret = requests.post(url, data=xml_data, headers={'Content-Type': 'text/xml'})
-#                 ret.encoding = 'utf8'
-#                 DOMTree = xmldom.parseString(ret.text)
-#                 collection = DOMTree.documentElement
-#                 return_code = collection.getElementsByTagName("return_code")[0].childNodes[0].data
-#                 print('===============================return_code======================> ', return_code)
-#                 if return_code == 'SUCCESS':
-#                     dingDanobjs.update(
-#                             theOrderStatus=8,   # 支付成功 改订单状态成功
-#                             stopDateTime=nowDate
-#                         )
-##
-#
-#
-#
-#
-#
-#         else:
-#             dingDanobjs.update(
-#                 theOrderStatus=9,  # 支付失败 改订单状态失败
-#                 stopDateTime=nowDate
-#             )
-#
-    response.code = 200
-    response.data = ''
-    response.msg = ''
-    return JsonResponse(response.__dict__)
 
 
 @csrf_exempt
-@account.is_token(models.Userprofile)
-def yuZhiFu(request):
-    if request.method == 'POST':
-        url =  'https://api.mch.weixin.qq.com/pay/unifiedorder'  # 微信支付接口
+def weixin_pay(request, oper_type):
+    response = Response.ResponseObj()
+
+    # 预支付
+    if oper_type == 'yuZhiFu':
+        pub_obj = pub() # 实例 公共函数
+        url = 'https://api.mch.weixin.qq.com/pay/unifiedorder'  # 微信支付接口
 
         user_id = request.GET.get('user_id')
 
         userObjs = models.Userprofile.objects.filter(id=user_id)
 
-        openid = userObjs[0].openid                                 # openid  用户标识
-
+        openid = userObjs[0].openid  # openid  用户标识
+        print('openid-----------> ', openid)
         total_fee = 100
-        ymdhms = time.strftime("%Y%m%d%H%M%S", time.localtime())        # 年月日时分秒
-        shijianchuoafter5 = str(int(time.time() * 1000))[8:]            # 时间戳 后五位
+        ymdhms = time.strftime("%Y%m%d%H%M%S", time.localtime())  # 年月日时分秒
+        shijianchuoafter5 = str(int(time.time() * 1000))[8:]  # 时间戳 后五位
         dingdanhao = str(ymdhms) + shijianchuoafter5 + str(random.randint(10, 99))
 
         result_data = {
-            'appid': 'wx0f55c67abd0ebf3d',                  # appid
-            'mch_id': '1488841842',                         # 商户号
-            'nonce_str': generateRandomStamping(),          # 32位随机值a
+            'appid': 'wx0f55c67abd0ebf3d',  # appid
+            'mch_id': '1488841842',  # 商户号
+            'nonce_str': pub_obj.generateRandomStamping(),  # 32位随机值a
             'openid': openid,
-            'body': 'zhuge-vip',                            # 描述
-            'out_trade_no': dingdanhao,                     # 订单号
-            'total_fee': total_fee,                         # 金额
-            'spbill_create_ip': '0.0.0.0',                  # 终端IP
+            'body': 'zhuge-vip',  # 描述
+            'out_trade_no': dingdanhao,  # 订单号
+            'total_fee': total_fee,  # 金额
+            'spbill_create_ip': '0.0.0.0',  # 终端IP
             'notify_url': 'http://tianyan.zhangcong.top/api/wxpay',
             'trade_type': 'JSAPI'
-            }
+        }
 
-        stringSignTemp = shengchengsign(result_data)
+        stringSignTemp = pub_obj.shengchengsign(result_data)
         print('stringSignTemp-------> ', stringSignTemp)
-        result_data['sign'] = md5(stringSignTemp).upper()
+        result_data['sign'] = pub_obj.md5(stringSignTemp).upper()
         print('result_data------------> ', result_data)
-        xml_data = toXml(result_data)
+        xml_data = pub_obj.toXml(result_data)
         print('xml_data--> ', xml_data)
         ret = requests.post(url, data=xml_data, headers={'Content-Type': 'text/xml'})
         ret.encoding = 'utf8'
         print(ret.text)
+
         # DOMTree = xmldom.parseString(ret.text)
         # collection = DOMTree.documentElement
         # data = ['return_code', 'return_msg']
@@ -169,7 +106,7 @@ def yuZhiFu(request):
         #     resultData = xmldom_parsing.xmldom(collection, data)
         #     response.code = 200
         #     response.msg = '预支付请求成功'
-            # 预支付成功 创建订单
+        # 预支付成功 创建订单
         #     if not fukuan: # 判断是否已经存在订单
         #         dingDanObjs = models.zgld_shangcheng_dingdan_guanli.objects
         #         date_time = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
@@ -218,9 +155,11 @@ def yuZhiFu(request):
         #     response.code = 301
         #     response.data = ''
 
+    # 微信回调
+    elif oper_type == 'wxpay':
+        print('------------------------------回调')
+        response.code = 200
 
-    else:
-        response.code = 402
-        response.msg = '请求异常'
-        response.data = ''
+
+
     return JsonResponse(response.__dict__)
