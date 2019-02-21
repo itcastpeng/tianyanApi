954cc665a6d53efde429efabf1f61b26b692d614
congzhang <652314341@qq.com>
2019-02-21 15:09:08 +0800

    优化代码

--- api/forms/posters.py
+++ api/forms/posters.py
@@ -159,7 +159,7 @@ class UpdatePosterInfoForm(forms.Form):
 
     def clean_phone(self):
         phone = self.data.get('phone')
-        phone_pat = re.compile('^(13\d|14[5|7]|15\d|166|17[3|6|7]|18\d)\d{8}$')
+        phone_pat = re.compile('^(13\\d|14[5|7]|15\\d|166|17[3|6|7]|18\\d)\\d{8}$')
         res = re.search(phone_pat, phone)
         if res:
             return phone
