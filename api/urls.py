48aef25215e8e29de23e09dab5483ddb28a03e3f
zhaoxinpeng <15531506906@sina.cn>
2019-03-11 13:50:41 +0800

    客户  登录测试

--- api/urls.py
+++ api/urls.py
@@ -48,7 +48,6 @@ urlpatterns = [
     # 微店管理
     url(r'^small_shop/(?P<oper_type>\w+)/(?P<o_id>\d+)', small_shop.small_shop_oper),
     url(r'^small_shop', small_shop.small_shop),
-    url(r'^customer_small_shop', small_shop.customer_small_shop), # 客户查看微店
 
     # ---------------------------------图片上传---------------------------------
     url(r'^upload_shard$', upload_file.upload_shard),     # 分片
