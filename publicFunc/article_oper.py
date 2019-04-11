b9b3a71e8be7f05e0981edfbd1cb44a6dd75f510
zhaoxinpeng <15531506906@sina.cn>
2019-04-11 16:32:28 +0800

    爬取文章

--- publicFunc/article_oper.py
+++ publicFunc/article_oper.py
@@ -41,7 +41,7 @@ def give_like(article_id, customer_id=None, user_id=None):
 
 # 创建文章
 def add_article_public(data, classify_id=None):
-    print('data--------------data-------创建文章-----》 ', data)
+    print('data--------------data-------创建文章-----》 ', data.get('usercreate_user_id'),  data.get('title'))
     article_objs = models.Article.objects.filter(
         title=data.get('title'),
         create_user_id=data.get('usercreate_user_id'),
