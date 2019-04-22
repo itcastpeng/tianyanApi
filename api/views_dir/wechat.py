cf5b695665c5b7f37b8d8e6365b05a19480798db
zhaoxinpeng <15531506906@sina.cn>
2019-04-22 21:12:33 +0800

    推送随机文章

--- api/views_dir/wechat.py
+++ api/views_dir/wechat.py
@@ -218,7 +218,7 @@ def wechat(request):
                     data = get_ent_info(user_obj.id)
                     weichat_api_obj = WeChatApi(data)
                     post_data = {
-                        "touser":"".format(openid),
+                        "touser":"%s" % openid,
                         "msgtype": "text",
                         "text": {
                             # "url":'http://www.baidu.com',
