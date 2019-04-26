ee6f7b5eba872f760d1a7b3bbde614217522cafc
zhaoxinpeng <15531506906@sina.cn>
2019-04-26 17:15:23 +0800

    天眼数据同步

--- tianyan_celery/celery.py
+++ tianyan_celery/celery.py
@@ -16,11 +16,11 @@ CELERYD_FORCE_EXECV = True    # 非常重要,有些情况下可以防止死锁
 CELERYD_MAX_TASKS_PER_CHILD = 100    # 每个worker最多执行万100个任务就会被销毁，可防止内存泄露
 app.conf.beat_schedule = {
 
-    # 判断静态文件是否有数据 如果有上传到线上服务器 每一小时执行一次
+    # 判断静态文件是否有数据 如果有上传到线上服务器 每10分钟执行一次
     'upload_day_eye':{
         'task':'tianyan_celery.tasks.upload_day_eye',
         # 'schedule': crontab("0", '*/1', '*', '*', '*'),  # 此处跟 linux 中 crontab 的格式一样
-        'schedule': crontab(minute=0, hour=1),  # 此处跟 linux 中 crontab 的格式一样
+        'schedule': crontab(minute=10),  # 此处跟 linux 中 crontab 的格式一样
         'args':[] # # 传入任务函数的参数,可以是一个列表或元组,如果函数没参数则为空列表或空元组
     },
 
