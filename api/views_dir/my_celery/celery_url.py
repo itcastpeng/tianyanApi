c88831da86eadae2c012f9f9b48d3687b3cbbe14
zhaoxinpeng <15531506906@sina.cn>
2019-05-07 11:26:18 +0800

    客户查看文章 提醒到用户

--- api/views_dir/my_celery/celery_url.py
+++ api/views_dir/my_celery/celery_url.py
@@ -307,7 +307,7 @@ def customer_view_articles_send_msg(request):
         user_info = get_ent_info(user_id)
         weixin_objs = WeChatApi(user_info)
         post_data = {
-                    "touser": user_info.openid,
+                    "touser": user_info.get('openid'),
                     "msgtype": "text",
                     "text": {
                         "content": '有人看了您的{}\n\n\n《{}》\n\n赶快点击 *天眼* 查看吧！'.format(check_type,title)
