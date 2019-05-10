539a495cbb36113af8d24dd7c93ab9a27c55cdc0
zhaoxinpeng <15531506906@sina.cn>
2019-05-10 17:02:03 +0800

    优化图片查询

--- api/views_dir/prepaidManagement.py
+++ api/views_dir/prepaidManagement.py
@@ -371,7 +371,7 @@ def payback(request):
                                 # 创建充值分销人应得钱数日志
                                 models.distribute_money_log.objects.create(
                                     user_id=renewal_log_obj.create_user_id,
-                                    inviter_id=inviter_id,
+                                    inviter_id=two_inviter_id,
                                     price=price,
                                     money=cumulative_amount
                                 )
